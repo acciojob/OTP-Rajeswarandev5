d773719d5e9d32688f8b5310ed7d4b5840dcf6d5
Rajeswarandev5 <rajeswaran.dev5@gmail.com>
2026-08-25 16:46:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll(".code");
+
+codes.forEach((input, index) => {
+
+  // Move to next input after entering a digit
+  input.addEventListener("input", function () {
+
+    // Allow only numbers
+    this.value = this.value.replace(/\D/g, "");
+
+    if (this.value !== "" && index < codes.length - 1) {
+      codes[index + 1].focus();
+    }
+  });
+
+
+  // Backspace behavior
+  input.addEventListener("keydown", function (event) {
+
+    if (event.key === "Backspace") {
+
+      // If current field contains a digit, delete it
+      if (this.value !== "") {
+        this.value = "";
+        event.preventDefault();
+        return;
+      }
+
+      // If current field is empty, move to previous field
+      if (index > 0) {
+        codes[index - 1].focus();
+        codes[index - 1].value = "";
+      }
+    }
+  });
+});
+
+
+// Automatically focus the first field
+codes[0].focus();
